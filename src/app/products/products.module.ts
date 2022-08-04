365f0f94241c15a9278fa5f8f78378aade373805
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2022-08-03 20:20:13 -0500

    .

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -5,11 +5,10 @@ import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsListComponent } from './pages/products-list/products-list.component';
 import { ProductCardComponent } from './components/product-card/product-card.component';
 
-
 @NgModule({
   declarations: [
     ProductsListComponent,
-    ProductCardComponent
+    ProductCardComponent,
   ],
   imports: [
     CommonModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,8 +6,7 @@ import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-
-    NavbarComponent
+    NavbarComponent,
   ],
   imports: [
     CommonModule,
