d332620d853c76f5ccf5c29875b995ec3edccae6
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2022-08-05 12:25:00 -0500

    navbar in progress

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  public activeMenu: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleMenu(): void {
+    this.activeMenu = !this.activeMenu;
   }
 
 }
